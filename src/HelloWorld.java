991690974ea786e3885760d1cd4864a5e7fe6ae9
MyPhantomBrother <gluhovskiy_art@mail.ru>
2017-09-28 20:57:00 +0300

    Added methods in origin/master project

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello from initial project!!!");
+    }
+
+    //Method added
+    public int countSmth(int a) {
+        return a * 2;
+    }
+}
